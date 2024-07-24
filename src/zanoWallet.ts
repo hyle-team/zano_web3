a5a63b5d0e8de5db2fbb1e4a04194cc18b2236de
jejolare <jejolare@gmail.com>
2024-07-24 22:53:06 +0700

    debug

--- src/zanoWallet.ts
+++ src/zanoWallet.ts
@@ -118,6 +118,10 @@ class ZanoWallet {
 
         const existingWalletValid = existingWallet && existingWallet.address === walletData.address;
 
+        console.log('existingWalletValid', existingWalletValid);
+        console.log('existingWallet', existingWallet);
+        console.log('walletData', walletData);
+        
         if (existingWalletValid) {
             nonce = existingWallet.nonce;
             signature = existingWallet.signature;
